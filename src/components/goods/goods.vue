d1b37dfc0b139eb34e8a44e8a99f809d61912519
Li, Fiona <fiona.li@sap.com>
2018-04-04 00:21:55 +0800

    first commit

---
+++ src/components/goods/goods.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>我是goods</div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+</style>
---
+++ src/components/ratings/ratings.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>我是ratings</div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+</style>
